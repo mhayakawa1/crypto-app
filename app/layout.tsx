7e517901316681f9061d5d82c8316d3ebe17ab96
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-01-16 18:35:47 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Link href="/">Home</Link>
+        <Link href="/portfolio">Portfolio</Link>
         {children}
       </body>
     </html>
