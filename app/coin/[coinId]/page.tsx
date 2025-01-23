60b00d3685beaebf553c1e65ec82d69abc16f72e
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-01-22 21:30:21 -0700

    git

--- app/coin/[coinId]/page.tsx
+++ app/coin/[coinId]/page.tsx
@@ -1,8 +1,14 @@
-export default function Coin({params}: {params: {coinId: 'string'}}) {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        {params.coinId}
-      </div>
-    );
-  }
+"use client";
+import { use } from 'react';
+type Params = Promise<{ coinId: string }>
+
+export default function GPSFix(props: { params: Params }) {
+  const params = use(props.params);
+  const coinId = params.coinId;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {coinId}
+    </div>
+  );
+}
   
