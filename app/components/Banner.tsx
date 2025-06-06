03cfe5d55c6f92474f6a50c7c19d782462f2dce9
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-06-05 18:30:05 -0700

    Create viewReducer, ass responsive styling

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -4,18 +4,20 @@ import { Progress } from "../../components/ui/progress";
 import { formatNumber } from "@/lib/format/formatNumber";
 import { useGlobalQuery } from "@/lib/features/api/apiSlice";
 import { useAppSelector } from "@/lib/hooks";
+import BannerItem from "./BannerItem";
 import FlashCircle from "../../src/icons/Flash_Circle.svg";
 import ArrowUpGreen from "../../src/icons/Arrow_Up_Green.svg";
+import BTC from "../../src/icons/BTC.svg";
+import ETH from "../../src/icons/ETH.svg";
 import Image from "next/image";
 
-const BannerItem = (props: { children: any }) => {
-  const { children } = props;
-  return <div className="flex items-center gap-[8px]">{children}</div>;
-};
-
 const Banner = () => {
-  const { currency: currency } = useAppSelector((state) => state.currency);
+  const { currency: currency, symbol } = useAppSelector(
+    (state) => state.currency
+  );
   const [errorMessage, setErrorMessage] = useState("");
+  const view = useAppSelector((state) => state.view);
+  const mobileView = view[0].mobileView;
   const [bannerData, setBannerData] = useState({
     coins: 0,
     marketCap: 0,
@@ -56,46 +58,80 @@ const Banner = () => {
   }, [data, currency, error, isError, isSuccess]);
 
   return (
-    <div className="flex justify-center text-xs bg-[--dark-slate-blue] dark:bg-[--haiti] w-full py-[16px] border-y border-[#343046]">
-      {isLoading && <span>Loading...</span>}
+    <div className="flex justify-center text-xs bg-[--dark-slate-blue] dark:bg-[--haiti] w-full py-[16px] max-sm:py-[8px] lg:2xl:py-[32px] border-y border-[--space-cadet]">
+      {isLoading && <span className="lg:2xl:text-2xl">Loading...</span>}
       {isSuccess && (
-        <div className="flex justify-center gap-[4vw]">
+        <div className="w-full flex justify-center max-sm:justify-evenly sm:gap-[4vw]">
+          {!mobileView && (
+            <BannerItem>
+              <Image
+                src={FlashCircle}
+                alt=""
+                className="lg:2xl:h-[24px] lg:2xl:w-[24px]"
+              />
+              <span className="text-[#D1D1D1] lg:2xl:text-2xl">Coins</span>
+              <span className="lg:2xl:text-2xl">{bannerData.coins}</span>
+            </BannerItem>
+          )}
+          {!mobileView && (
+            <BannerItem>
+              <Image
+                src={ArrowUpGreen}
+                alt=""
+                className="lg:2xl:h-[8px] w-auto"
+              />
+              <span className="lg:2xl:text-2xl">
+                {formatNumber(bannerData.marketCap, "")}
+              </span>
+            </BannerItem>
+          )}
           <BannerItem>
-            <Image src={FlashCircle} alt="" />
-            <span className="text-[#D1D1D1]">Coins</span>
-            <span>{bannerData.coins}</span>
-          </BannerItem>
-          <BannerItem>
-            <Image src={ArrowUpGreen} alt="" />
-            <span>{formatNumber(bannerData.marketCap, "")}</span>
-          </BannerItem>
-          <BannerItem>
-            <span>{formatNumber(bannerData.totalVolume, "")}</span>
+            <span className="lg:2xl:text-2xl">
+              {formatNumber(bannerData.totalVolume, symbol)}
+            </span>
             <Progress
-              className={"bg-[rgb(255,255,255,.4)] w-[53px] h-[6px]"}
+              className="bg-[rgb(255,255,255,.4)] w-[53px] max-sm:w-[42px] lg:2xl:w-[106px] h-[6px] lg:2xl:h-[12px]"
               value={bannerData.totalVolume}
               color={"bg-[--background]"}
-            ></Progress>
+            />
           </BannerItem>
           <BannerItem>
-            <span>{Number(bannerData.btc.toFixed(2))}%</span>
+            <span className="flex items-center gap-[2px] lg:2xl:text-2xl">
+              <Image
+                src={BTC}
+                alt=""
+                className="w-auto h-[12px] lg:2xl:h-[24px] aspect-square"
+              />
+              {Math.round(Number(bannerData.btc))}%
+            </span>
             <Progress
-              className={"bg-[rgb(255,255,255,.4)] w-[53px] h-[6px]"}
+              className={
+                "bg-[rgb(255,255,255,.4)] w-[53px] max-sm:w-[42px] lg:2xl:w-[106px] h-[6px] lg:2xl:h-[12px] max-sm:text-xs"
+              }
               value={bannerData.btc}
               color={"bg-[#F7931A]"}
-            ></Progress>
+            />
           </BannerItem>
           <BannerItem>
-            <span>{Number(bannerData.eth.toFixed(2))}%</span>
+            <span className="flex items-center gap-[2px] lg:2xl:gap-[4px] lg:2xl:text-2xl">
+              <Image
+                src={ETH}
+                alt=""
+                className="w-auto h-[12px] lg:2xl:h-[24px] aspect-square"
+              />
+              {Math.round(Number(bannerData.eth))}%
+            </span>
             <Progress
-              className={"bg-[rgb(255,255,255,.4)] w-[53px] h-[6px]"}
+              className={
+                "bg-[rgb(255,255,255,.4)] w-[53px] max-sm:w-[42px] lg:2xl:w-[106px] h-[6px] lg:2xl:h-[12px] max-sm:text-xs"
+              }
               value={bannerData.eth}
               color={"bg-[#849DFF]"}
-            ></Progress>
+            />
           </BannerItem>
         </div>
       )}
-      {isError && <span>{errorMessage}</span>}
+      {isError && <span className="lg:2xl:text-2xl">{errorMessage}</span>}
     </div>
   );
 };
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,6 +11,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { toggleView } from "@/lib/features/view/viewSlice";
 import { Input } from "@/components/ui/input";
 import { switchCurrency } from "@/lib/features/currency/currencySlice";
 import { useAllCoinsQuery } from "@/lib/features/api/apiSlice";
@@ -32,6 +33,8 @@ import SearchWhite from "../../src/icons/Search_White.svg";
 
 const Navbar = () => {
   const darkActive = useAppSelector((state) => state.theme)[0].darkActive;
+  const view = useAppSelector((state) => state.view);
+  const mobileView = view[0].mobileView;
   const dispatch = useAppDispatch();
   const { currency } = useAppSelector((state) => state.currency);
   const [changeCurrency, setChangeCurrency] = useState(true);
@@ -39,7 +42,6 @@ const Navbar = () => {
   const [homeIcon, setHomeIcon] = useState(HomeWhite);
   const [resultsVisible, setResultsVisible] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const [mobileView, setMobileView] = useState(false);
   const [initialRender, setInitialRender] = useState(true);
   const selectItems = [
     { name: "usd", icon: USD },
@@ -102,14 +104,13 @@ const Navbar = () => {
     setChangeCurrency(false);
 
     const handleResize = () => {
-      if (window.innerWidth < 640 && !mobileView) {
-        setMobileView(true);
-      } else if (window.innerWidth >= 640 && mobileView) {
-        setMobileView(false);
+      if (window.innerWidth < 640) {
+        dispatch(toggleView(true));
+      } else if (window.innerWidth >= 640) {
+        dispatch(toggleView(false));
       }
     };
     window.addEventListener("resize", handleResize);
-
     if (initialRender) {
       handleResize();
       setInitialRender(false);
@@ -124,55 +125,59 @@ const Navbar = () => {
           alt=""
           width={36}
           height={20}
-          className="m-auto max-md:w-[28px] max-md:h-[16px]"
+          className="m-auto max-md:w-[28px] lg:2xl:w-[72px] max-md:h-[16px]"
         />
-        <h1 className="text-[--dark-slate-blue] dark:text-white text-xl max-md:hidden font-bold">Logoipsm</h1>
+        <h1 className="text-[--dark-slate-blue] dark:text-white text-xl lg:2xl:text-4xl max-md:hidden font-bold">
+          Logoipsm
+        </h1>
       </div>
-      {!mobileView && <div className="flex justify-between gap-[2vw]">
-        <LinkButton
-          src={darkActive ? homeIcon : HomeBlue}
-          path=""
-          name="Home"
-          homeActive={homeActive}
-          toggleHomeActive={toggleHomeActive}
-        />
-        <LinkButton
-          src={darkActive ? PortfolioWhite : PortfolioBlue}
-          path="portfolio"
-          name="Portfolio"
-          homeActive={homeActive}
-          toggleHomeActive={toggleHomeActive}
-        />
-      </div>}
+      {!mobileView && (
+        <div className="flex justify-between gap-[2vw]">
+          <LinkButton
+            src={darkActive ? homeIcon : HomeBlue}
+            path=""
+            name="Home"
+            homeActive={homeActive}
+            toggleHomeActive={toggleHomeActive}
+          />
+          <LinkButton
+            src={darkActive ? PortfolioWhite : PortfolioBlue}
+            path="portfolio"
+            name="Portfolio"
+            homeActive={homeActive}
+            toggleHomeActive={toggleHomeActive}
+          />
+        </div>
+      )}
       <div className="flex justify-between items-center gap-[1vw]">
         <div
           onBlur={hideResults}
-          className={`relative gap-[12px] m-0 w-auto h-auto ${
+          className={`relative gap-[12px] lg:2xl:gap-[24px] m-0 w-auto h-auto ${
             resultsVisible
-              ? "rounded-tl-[6px] rounded-tr-[6px] rounded-bl-none rounded-br-none"
-              : "rounded-[6px]"
+              ? "rounded-tl-[6px] lg:2xl:rounded-tl-[12px] rounded-tr-[6px] lg:2xl:rounded-tr-[12px] rounded-bl-none rounded-br-none"
+              : "rounded-[6px] lg:2xl:rounded-[12px]"
           } h-fit w-fit bg-[--lavender] dark:bg-[--mirage]`}
         >
           <Image
             src={darkActive ? SearchWhite : SearchBlue}
             alt=""
-            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            className="absolute lg:2xl:w-[40px] lg:2xl:h-[40px] top-1/2 left-[12px] max-sm:left-1/2 lg:2xl:left-[24px] max-sm:transform max-sm:-translate-x-1/2 -translate-y-1/2"
           />
           <Input
             onChange={(event) => searchCoins(event.target.value.toLowerCase())}
             value={searchValue}
             placeholder={mobileView ? "" : "Search..."}
-            className={`rounded-tl-[6px] rounded-tr-[6px] ${
+            className={`rounded-tl-[6px] lg:2xl:rounded-tl-[12px] rounded-tr-[6px] lg:2xl:rounded-tr-[12px] ${
               resultsVisible
                 ? "rounded-bl-none rounded-br-none"
-                : "rounded-bl-[6px] rounded-br-[6px]"
-            } h-[48px] max-md:h-[36px] max-md:w-[100px] max-sm:w-[36px] pl-[44px] max-md:pl-[32px] max-sm:pl-0 flex justify-center items-center bg-transparent text-sm max-md:text-xs outline-none text-[--dark-slate-blue] border-none dark:text-white dark:border dark:border-[--dark-gunmetal]`}
+                : "rounded-bl-[6px] lg:2xl:rounded-bl-[12px] rounded-br-[6px] lg:2xl:rounded-br-[12px]"
+            } h-[48px] max-md:h-[36px] lg:2xl:h-[96px] w-[200px] max-md:w-[100px] max-sm:w-[36px] lg:2xl:w-[400px] pl-[44px] max-md:pl-[32px] max-sm:pl-0 lg:2xl:pl-[88px] flex justify-center items-center bg-transparent text-sm max-md:text-xs lg:2xl:text-3xl outline-none text-[--dark-slate-blue] border-none dark:text-white dark:border dark:border-[--dark-gunmetal]`}
           />
           {resultsVisible &&
             ((isLoading && <ResultsEmpty message="Loading..." />) ||
               (isError && <ResultsEmpty message={error.toString()} />) ||
               (resultsVisible && isSuccess ? (
-                <ul className="absolute z-10 pb-[8px] rounded-bl-[6px] rounded-br-[6px] w-full text-[--dark-slate-blue] dark:text-white bg-[--lavender] dark:border dark:border-[--dark-gunmetal] dark:border-t-0 dark:bg-[--mirage]">
+                <ul className="absolute z-10 pb-[8px] lg:2xl:pb-[16px] rounded-bl-[6px] lg:2xl:rounded-bl-[12px] rounded-br-[6px] lg:2xl:rounded-br-[12px] w-full text-[--dark-slate-blue] dark:text-white bg-[--lavender] dark:border dark:border-[--dark-gunmetal] dark:border-t-0 dark:bg-[--mirage]">
                   {data
                     .map((element: any) => {
                       return { id: element.id, name: element.name };
@@ -183,10 +188,10 @@ const Navbar = () => {
                     .map((result: any) => (
                       <li
                         key={result.id}
-                        className="px-[8px] py-[6px] hover:bg-white"
+                        className="px-[8px] lg:2xl:px-[16px] py-[6px] lg:2xl:py-[12px] hover:bg-white"
                       >
                         <Link
-                          className="flex items-center gap-[16px]"
+                          className="flex items-center gap-[16px] lg:2xl:gap-[32px] lg:2xl:text-3xl"
                           href={`/coin/${result.id}`}
                           onClick={() => searchCoins("")}
                         >
@@ -199,10 +204,10 @@ const Navbar = () => {
         </div>
         {!changeCurrency && (
           <Select defaultValue={currency} onValueChange={handleChange}>
-            <SelectTrigger className="w-[108px] max-md:w-[84px] h-[48px] max-md:h-[36px] px-[16px] max-md:px-[8px] bg-[--lavender] text-[--dark-slate-blue] border-none dark:text-white dark:border dark:border-[--dark-gunmetal] dark:bg-[--mirage]">
+            <SelectTrigger className="w-[108px] max-md:w-[84px] lg:2xl:w-[216px] h-[48px] max-md:h-[36px] lg:2xl:h-[96px] px-[16px] max-md:px-[8px] lg:2xl:px-[32px] bg-[--lavender] text-[--dark-slate-blue] border-none dark:text-white dark:border dark:border-[--dark-gunmetal] dark:bg-[--mirage]">
               <SelectValue className="flex justify-center items-center" />
             </SelectTrigger>
-            <SelectContent className="w-[108px] max-md:w-[84px] bg-[--lavender] border-none dark:border dark:border-[--dark-gunmetal] dark:bg-[--mirage]">
+            <SelectContent className="w-[108px] max-md:w-[84px] lg:2xl:w-[216px] bg-[--lavender] border-none dark:border dark:border-[--dark-gunmetal] dark:bg-[--mirage]">
               <SelectGroup className="w-full bg-none text-[--dark-slate-blue] dark:text-white">
                 {selectItems.map((item: any) => (
                   <SelectItem
