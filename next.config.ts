407f925f716bb680e9aa55f013eb9b7c7065fb5b
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-06-06 20:27:37 -0700

    Fix deployment error 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactComponentAnnotation: {
+    enabled: process.env.NODE_ENV === "development",
+  },
 };
 
 export default nextConfig;
