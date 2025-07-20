2af9e918083ff428b30dbcd42e59a54056de78ba
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-07-20 11:40:59 -0700

    Update HighLow styling

--- app/components/HighLow.tsx
+++ app/components/HighLow.tsx
@@ -6,22 +6,22 @@ import ArrowDownRed from "../../src/icons/Arrow_Down_Red.svg";
 const HighLow = (props: { price: string; time: string; high: boolean }) => {
   const { price, time, high } = props;
   return (
-    <li className="high-low flex justify-start gap-[16px] lg:2xl:gap-[32px] h-[44px] lg:2xl:h-[88px]">
+    <li className="flex justify-start gap-[16px] lg:2xl:gap-[24px] h-[44px] lg:2xl:h-[66px]">
       <Image
         width="0"
         height="0"
-        className="w-[9px] max-sm:w-[6px] lg:2xl:w-[18px] h-auto mt-[20px] lg:2xl:my-auto"
+        className="w-[8px] max-sm:w-[6px] lg:2xl:w-[12px] h-auto my-auto"
         src={high ? ArrowUpGreen.src : ArrowDownRed.src}
         alt=""
       />
-      <ul className="text-base max-sm:text-sm lg:2xl:text-3xl text-nowrap">
+      <ul className="text-base max-sm:text-sm lg:2xl:text-2xl text-nowrap">
         <li>
           All time {high ? "high" : "low"}:{" "}
-          <span className="text-xl max-sm:text-base lg:2xl:text-4xl">
+          <span className="text-xl max-sm:text-base lg:2xl:text-3xl">
             {price}
           </span>
         </li>
-        <li className="date text-sm max-sm:text-xs lg:2xl:text-xl text-[#b9b9ba]">
+        <li className="date text-sm max-sm:text-xs lg:2xl:text-lg text-[#b9b9ba]">
           {time}
         </li>
       </ul>
