961f8ef43c984be05a4dde1943d4b509c5aab9f4
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-07-21 14:05:44 -0700

    Update responsive styling

--- app/components/GradientBorderButton.tsx
+++ app/components/GradientBorderButton.tsx
@@ -25,12 +25,12 @@ const GradientBorderButton = (props: {
         active
           ? "p-[1px] lg:2xl:p-[2px] bg-gradient-to-b from-[--soft-blue] to-[--perano] dark:to-[--american-blue] shadow-[4px_4px_15px_2px_#7878fa26]"
           : `${background}`
-      } rounded-[6px] lg:2xl:rounded-[12px] flex ${buttonClasses}`}
+      } rounded-[6px] lg:2xl:rounded-[9px] flex ${buttonClasses}`}
     >
       <span
         className={`${
           active ? "bg-[--perano] dark:bg-[--american-blue]" : "bg-none"
-        } w-full h-full rounded-[5px] lg:2xl:rounded-[10px] flex justify-center items-center ${spanClasses}`}
+        } w-full h-full rounded-[4px] lg:2xl:rounded-[6px] flex justify-center items-center ${spanClasses}`}
       >
         {children ? children : text}
       </span>
