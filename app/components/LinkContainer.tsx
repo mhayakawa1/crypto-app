a2c972a16cec166abd66fbb8b330770bb40392fd
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-08-04 12:16:00 -0700

    Add link copy popover

--- app/components/LinkContainer.tsx
+++ app/components/LinkContainer.tsx
@@ -1,4 +1,9 @@
 "use client";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import Image from "next/image";
 import LinkWhite from "../../src/icons/Link_White.svg";
 import CopyWhite from "../../src/icons/Copy_White.svg";
@@ -28,17 +33,21 @@ const LinkContainer = (props: { link: string; sliceIndex: number }) => {
       <p className="mx-[16px] max-sm:mx-0 lg:2xl:mx-[24px] w-full text-center text-sm lg:2xl:text-xl truncate">
         {shortenedLink}
       </p>
-      <button
-        title="Copy link"
-        onClick={copyText}
-        className="flex items-center justify-center bg-[--perano] dark:bg-transparent aspect-square h-[24px] lg:2xl:h-[36px] p-[4px] lg:2xl:p-[6px] rounded-[4px] lg:2xl:rounded-[6px]"
-      >
-        <Image
-          src={CopyWhite}
-          alt=""
-          className="w-[20px] lg:2xl:w-[30px] h-auto"
-        />
-      </button>
+      <Popover>
+        <PopoverTrigger>
+          <button
+            onClick={copyText}
+            className="flex items-center justify-center bg-[--perano] dark:bg-transparent aspect-square h-[24px] lg:2xl:h-[36px] p-[4px] lg:2xl:p-[6px] rounded-[4px] lg:2xl:rounded-[6px]"
+          >
+            <Image
+              src={CopyWhite}
+              alt=""
+              className="w-[20px] lg:2xl:w-[30px] h-auto"
+            />
+          </button>
+        </PopoverTrigger>
+        <PopoverContent className='border-none w-fit h-fit py-1 px-2 flex justify-center items-center bg-[--perano] dark:bg-[--dark-slate-blue] text-xs text-white'><span>Link Copied!</span></PopoverContent>
+      </Popover>
     </Panel>
   );
 };
